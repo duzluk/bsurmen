ea46a9d55476c709f55043fe670b0183880364df
Burak Surmen <busurmen@gmail.com>
2020-03-19 15:27:05 +0300

    Added contact form features

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Form from "../common/form";
 import BackArrow from "../common/backArrow";
@@ -6,6 +6,22 @@ import ContactImage from "./contactImage";
 import NavBar from "./../common/navBar";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [mail, setMail] = useState("");
+  const [timeFrame, setTimeFrame] = useState("");
+  const [budget, setBudget] = useState(0);
+  const [msg, setMsg] = useState("");
+  const [radio, setRadio] = useState(undefined);
+
+  const handleForm = (name, mail, timeFrame, budget, msg, radio) => {
+    setName(name);
+    setMail(mail);
+    setTimeFrame(timeFrame);
+    setBudget(budget);
+    setMsg(msg);
+    setRadio(radio);
+  };
+
   return (
     <Container fluid>
       <Row>
@@ -15,7 +31,15 @@ const Contact = () => {
           <ContactImage />
         </Col>
         <Col xs={6}>
-          <Form />
+          <Form
+            handleForm={handleForm}
+            name={name}
+            mail={mail}
+            timeFrame={timeFrame}
+            budget={budget}
+            msg={msg}
+            radio={radio}
+          />
         </Col>
         <BackArrow />
       </Row>
--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,56 +1,103 @@
 import React from "react";
 import { Form, Row, Col, Button } from "react-bootstrap";
+import { Radio, RadioGroup } from "react-radio-group";
 
-const contactForm = () => {
+const contactForm = ({
+  handleForm,
+  name,
+  mail,
+  timeFrame,
+  budget,
+  msg,
+  radio
+}) => {
+  const handleOnChange = e => {};
+  const handleInput = event => {
+    handleForm(event.target.value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    handleForm(event.target.value);
+  };
+
   return (
     <React.Fragment>
       <h1>Need some help?</h1>
       <h1>ping me</h1>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Row>
           <Col>
             <Form.Label>What is your name?</Form.Label>
-            <Form.Control className="input" />
+            <Form.Control
+              className="input"
+              name="name"
+              value={name}
+              onChange={name => handleInput(name)}
+            />
           </Col>
           <Col>
             <Form.Group controlId="exampleForm.ControlInput1">
               <Form.Label>Your email</Form.Label>
-              <Form.Control type="email" className="input" />
+              <Form.Control
+                type="email"
+                name="mail"
+                className="input"
+                value={mail}
+                onChange={mail => handleInput(mail)}
+              />
             </Form.Group>
           </Col>
         </Row>
         <h1>project stage</h1>
-        {["radio"].map(type => (
-          <div key={`default-${type}`} className="mb-3">
-            <Form.Check
-              type={type}
-              id={`default-${type}`}
-              label={`Have product documentation or work in progress`}
-            />
 
-            <Form.Check
-              type={type}
-              label={`Project is already running (I have documentation or mockups etc.)`}
-              id={`default-${type}`}
-            />
+        <RadioGroup
+          name="stage"
+          onChange={e => handleOnChange(e)}
+          selectedValue={radio}
+        >
+          <div className="radio-button-background">
+            <Radio value={radio} className="radio-button" />
+            Have product documentation or work in progress
           </div>
-        ))}
+          <div className="radio-button-background">
+            <Radio value="radio2" className="radio-button" />
+            Project is already running (I have documentation or mockups etc.)
+          </div>
+        </RadioGroup>
 
         <Row>
           <Col>
             <Form.Label>What is your timeframe?</Form.Label>
-            <Form.Control className="input" />
+            <Form.Control
+              className="input"
+              name="timeFrame"
+              value={timeFrame}
+              onChange={timeFrame => handleInput(timeFrame)}
+            />
           </Col>
           <Col>
             <Form.Group controlId="exampleForm.ControlInput1">
               <Form.Label>Estimated budget?</Form.Label>
-              <Form.Control type="number" className="input" />
+              <Form.Control
+                type="number"
+                className="input"
+                value={budget}
+                onChange={budget => handleInput(budget)}
+              />
             </Form.Group>
           </Col>
         </Row>
         <Form.Group controlId="exampleForm.ControlTextarea1">
           <Form.Label>Add quick message here</Form.Label>
-          <Form.Control as="textarea" rows="3" className="input" />
+          <Form.Control
+            as="textarea"
+            rows="3"
+            name="msg"
+            className="input"
+            value={msg}
+            onChange={msg => handleInput(msg)}
+          />
         </Form.Group>
         <Button variant="primary" type="submit">
           Send it

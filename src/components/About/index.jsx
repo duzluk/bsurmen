dd1d4d1a710ab7a992a1726f123839bc06590907
Burak Surmen <busurmen@gmail.com>
2020-03-23 16:34:50 +0300

    Changed about page alignment and img

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,42 +1,21 @@
 import React, { useCallback } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import NavBar from "./../common/navBar";
 import AboutImage from "./aboutImage";
 import Typing, { Backspace, Delay, Reset, Speed } from "react-typing-animation";
+
 import { useSpring, animated as a, interpolate } from "react-spring";
 import "./style.css";
 
 const About = () => {
   const [{ st, xy }, set] = useSpring(() => ({ st: 0, xy: [0, 0] }));
-  const interpBg = xy.interpolate(
+  const moveSetting = xy.interpolate(
     (x, y) =>
       `perspective(600px) rotateY(${x / 60}deg) rotateX(${-y /
         60}deg) translate3d(-10%, -10%, 0)`
   );
-  const interpFace = st.interpolate(o => `translate(90,${105 + o / 4})`);
-  const interpEye = interpolate(
-    [st, xy],
-    (o, xy) =>
-      `translate(${xy[0] / 20 + 140},${xy[1] / 10 + 50 + o / 2}) scale(0.1)`
-  );
-  const interpIris = interpolate(
-    [st, xy],
-    (o, xy) => `translate(${xy[0] / 30},${xy[1] / 30 + -10 + o / 8})`
-  );
-  const interpPupil = interpolate(
-    [st, xy],
-    (o, xy) => `translate(${xy[0] / 25},${xy[1] / 25 + -10 + o / 8})`
-  );
-  const interpSpot = interpolate(
-    [st, xy],
-    (o, xy) => `translate(${8 + -xy[0] / 80},${-xy[1] / 80 + -10 + o / 8})`
-  );
-  const interpMouth = interpolate(
-    [st, xy],
-    (o, xy) =>
-      `translate(${xy[0] / 18 + 188},${xy[1] / 20 + 230 + o / 1.7}) scale(0.8)`
-  );
-  const interpHair = st.interpolate(o => `translate(79,${o / 4})`);
+
   const onMove = useCallback(
     ({ clientX: x, clientY: y }) =>
       set({ xy: [x - window.innerWidth / 2, y - window.innerHeight / 2] }),
@@ -45,11 +24,16 @@ const About = () => {
   const onScroll = useCallback(e => set({ st: e.target.scrollTop / 30 }), []);
 
   const AnimatedTypingComponent = () => (
-    <Typing style={{ height: "30px" }} speed="50">
-      <h2 style={{ fontSize: "50px" }}>
-        My name is Burak. I’m a front end web developer familiar with JavaScript
+    <Typing style={{ height: "30px" }} speed="80">
+      <h5
+        className="font-weight-bold"
+        style={{
+          fontSize: "50px"
+        }}
+      >
+        I'm Burak. I’m a front end web developer familiar with JavaScript
         frameworks.
-      </h2>
+      </h5>
     </Typing>
   );
 
@@ -59,41 +43,43 @@ const About = () => {
         <Row>
           <NavBar />
           <Col>
-            <Row>
-              <h2 style={{ margin: "2rem 1rem" }}>about me</h2>
-              <Col style={{ marginTop: "8%" }}>
+            <h2 className="pageTitle">about</h2>
+            <Row className="about">
+              <Col xs={6} md={5} className="aboutImg">
+                <AboutImage moveSetting={moveSetting} />
+              </Col>
+              <Col xs={12} md={5}>
                 <h4 style={{ fontSize: "40px" }}>Hey there!</h4>
                 <h2
                   style={{
-                    fontSize: "50px",
-                    fontWeight: "700"
+                    fontSize: "50px"
                   }}
+                  className="font-weight-bold"
                 >
-                  I'm Burak Sürmen. I’m a front end web developer familiar with
-                  JavaScript frameworks.
+                  <h5
+                    className="font-weight-bold"
+                    style={{
+                      fontSize: "50px"
+                    }}
+                  >
+                    I'm Burak. I’m a front end web developer familiar with
+                    JavaScript frameworks.
+                  </h5>
                 </h2>
-                <h6 style={{ fontSize: "25px" }}>
+                <h5>
                   Studying design gave me a good understanding with learning new
                   concepts and thinking methodologically, a skill that
-                  translates beautifully to writing code.
-                  <br />
-                  <br />I am excited to connect people through technology after
-                  having experience connecting people through design.
-                </h6>
-                <p
-                  style={{
-                    fontSize: "25px",
-                    fontWeight: "700"
-                  }}
-                >
+                  translates beautifully to writing code.I am excited to connect
+                  people through technology after having experience connecting
+                  people through design.
+                </h5>
+                <h4 className="font-weight-bold">
                   Interested in working together? <br />
                   Feel free to contact me for any project or collaboration.
-                </p>
-                <Button variant="danger">Contact me</Button>
-              </Col>
-
-              <Col xs={12} md={6}>
-                <AboutImage interpBg={interpBg} interpFace={interpFace} />
+                </h4>
+                <Link to="/contact">
+                  <Button variant="danger">Contact me</Button>
+                </Link>
               </Col>
             </Row>
           </Col>

577e12fc9e04201db6dbcaadbaf936b63f235b3d
Burak Surmen <busurmen@gmail.com>
2020-03-25 01:15:13 +0300

    Added not found page

--- src/components/NotFound/index.jsx
+++ src/components/NotFound/index.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import CustomCursor from "../common/customCursor";
 import CursorProvider from "../../Providers/cursorProvider";
+import { Link } from "react-router-dom";
+import Cactus from "./cactus";
+import "./style.css";
 
 const NotFound = () => {
   return (
     <CursorProvider>
-      <div className="notFound">
-        <h1>The page is not found.</h1>
-      </div>
+      <Row className="notFound">
+        <Col  xs={6} id="cactus">
+          <Cactus />
+        </Col>
+        <Col xs={6}>
+          <h1 className="font-weight-bold">Oops...</h1>
+          <h5>
+            We can’t find the page you are looking for.
+            <br /> May be the page is moved or deleted.
+          </h5>
+          <CustomCursor>
+            <Link to="/">
+              <Button variant="danger">Go back to Home</Button>
+            </Link>
+          </CustomCursor>
+        </Col>
+      </Row>
     </CursorProvider>
   );
 };
